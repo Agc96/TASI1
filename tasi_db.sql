b43d4992a9dfe7bfe18290e25999cfd9b0602e37
unknown <miguel.romero@pucp.edu.pe>
2018-11-17 10:40:55 -0500

    fixed?

--- tasi_db.sql
+++ tasi_db.sql
@@ -34,37 +34,37 @@ CREATE TABLE `usuario` (
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-DROP TABLE IF EXISTS 'curso';
-CREATE TABLE 'curso'(
-  'idcurso' int(11) NOT NULL AUTO_INCREMENT,
-  'nombrecurso' varchar(45) NOT NULL,
-  'codigocurso' varchar(7) NOT NULL,
-  PRIMARY KEY('idcurso')
+DROP TABLE IF EXISTS `curso`;
+CREATE TABLE `curso`(
+  `idcurso` int(11) NOT NULL AUTO_INCREMENT,
+  `nombrecurso` varchar(45) NOT NULL,
+  `codigocurso` varchar(7) NOT NULL,
+  PRIMARY KEY(`idcurso`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS 'software';
-CREATE TABLE 'software'(
-  'idsoftware' int(11) NOT NULL AUTO_INCREMENT,
-  'nombresoftware' varchar(45) NOT NULL,
-  'codigocurso' varchar(7) NOT NULL,
-  PRIMARY KEY('idsoftware')
+DROP TABLE IF EXISTS `software`;
+CREATE TABLE `software`(
+  `idsoftware` int(11) NOT NULL AUTO_INCREMENT,
+  `nombresoftware` varchar(45) NOT NULL,
+  `codigocurso` varchar(7) NOT NULL,
+  PRIMARY KEY(`idsoftware`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS 'sistemaOperativo';
-CREATE TABLE 'sistemaOperativo'(
-  'idsistemaOperativo' int(11) NOT NULL AUTO_INCREMENT,
-  'nombresistemaOperativo' varchar(45) NOT NULL,  
-  PRIMARY KEY('idsistemaOperativo')
+DROP TABLE IF EXISTS `sistemaOperativo`;
+CREATE TABLE `sistemaOperativo`(
+  `idsistemaOperativo` int(11) NOT NULL AUTO_INCREMENT,
+  `nombresistemaOperativo` varchar(45) NOT NULL,  
+  PRIMARY KEY(`idsistemaOperativo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS 'solicitud';
-CREATE TABLE 'solicitud'(
-  'idsolicitud' int(11) NOT NULL AUTO_INCREMENT,
-  'estado' varchar(15) NOT NULL,
-  'version' varchar(15) NOT NULL,
-  'observacionesProfesor' varchar(45) NOT NULL,
-  'observacionesSoporte' varchar(45) NOT NULL,
-  PRIMARY KEY('idsolicitud'),
+DROP TABLE IF EXISTS `solicitud`;
+CREATE TABLE `solicitud`(
+  `idsolicitud` int(11) NOT NULL AUTO_INCREMENT,
+  `estado` varchar(15) NOT NULL,
+  `version` varchar(15) NOT NULL,
+  `observacionesProfesor` varchar(45) NOT NULL,
+  `observacionesSoporte` varchar(45) NOT NULL,
+  PRIMARY KEY(`idsolicitud`),
   FOREIGN KEY (idsoftware)
     REFERENCES software(idsoftware)
     ON DELETE CASCADE,
